57dee30cbda24f787fcaf2171df930e4e1589943
qcrg <david-isupov@bk.ru>
2026-08-17 13:58:47 +0000

    fix(qr_service): fix auto-update qr code

--- lib/data/models/qr_data/qr_data.dart
+++ lib/data/models/qr_data/qr_data.dart
@@ -4,9 +4,15 @@ part "qr_data.freezed.dart";
 
 @freezed
 abstract class QrData with _$QrData {
+  const QrData._();
   const factory QrData({
     required String username,
     required String src,
     required DateTime expires,
   }) = _QrData;
+
+  bool isExpired([DateTime? now]) {
+    final now_date = now ?? DateTime.now().toUtc();
+    return !now_date.isBefore(this.expires);
+  }
 }
